940e8be90ef930158a172ad55fbec511c8e03fa5
Eric Seidel <gridaphobe@gmail.com>
2015-02-22 15:16:52 -0800

    can't use completely random hex

--- src/server.pl
+++ src/server.pl
@@ -1,6 +1,7 @@
 use strict;
 
 use Net::HTTPServer;
+use String::Random;
 
 my $server = new Net::HTTPServer();
 
@@ -17,8 +18,9 @@ sub gen_mp3
     my @types = ("audio/mpeg", "audio/MPA", "audio/mpa-robust");
     $res->Header("Content-Type", $types[rand @types]);
 
-    $res->Print(pack "H4", "FFFB"); # mp3 sync word + version id
-    $res->Print(pack "H4", "A040"); # other stuff, see http://en.wikipedia.org/wiki/MP3#File_structure
+    $res->Print(pack "H4", "FFFB");     # mp3 sync word + version id
+    $res->Print(pack "H4", "A040");     # other stuff, see http://en.wikipedia.org/wiki/MP3#File_structure
+    # $res->Print(pack "H4", rand_hex(4)); # other stuff, see http://en.wikipedia.org/wiki/MP3#File_structure
 
     my $len = int(rand 500*1024); # number of bytes
     for (my $i=0; $i < $len; $i++) {
@@ -26,6 +28,13 @@ sub gen_mp3
     }
 
     return $res;
+}
+
+sub rand_hex
+{
+    my $len = shift;
+    my $str = String::Random->new;
+    return $str->randregex("[0-9A-F]{$len}");
 }
 
 $server->Start();
